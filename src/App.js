9641ba2f0281088db3537cf49dee2d5d7740a567
Hojibola <hojibola003@gmail.com>
2022-03-20 18:12:56 +0500

    react counter

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Counter from "./components/counter/Counter"
+
+import './App.css';
+
+
+function App() {
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(counter + 1);
+    };
+    const decreament = () => {
+        setCounter(counter - 1);
+    };
+    const refresh = ()=> {
+        setCounter(0)
+    }
+    
+    return ( <div className = "App" > 
+   
+    <Counter refresh={refresh} decreament={decreament} increment={increment} counter={counter}/>
+   
+    </div> );
+}
+export default App;
